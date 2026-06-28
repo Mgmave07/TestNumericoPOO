8d1e77e3cdecb91815ae7678235c7acb8a2cabc6
CarlosSaav06 <cfugarte@uamv.edu.ni>
2026-06-28 00:15:48 -0600

    cambios a las clases del proyecto

---
+++ TestNumerico/src/main/java/ni/uam/edu/TestNumerico/enums/EstadoAplicacion.java
@@ -0,0 +1,7 @@
+package ni.uam.edu.TestNumerico.enums;
+
+public enum EstadoAplicacion {
+    PENDIENTE,
+    EN_PROCESO,
+    FINALIZADO
+}
---
+++ TestNumerico/src/main/java/ni/uam/edu/TestNumerico/enums/LetraOpcion.java
@@ -0,0 +1,8 @@
+package ni.uam.edu.TestNumerico.enums;
+
+public enum LetraOpcion {
+    A,
+    B,
+    C,
+    D
+}
